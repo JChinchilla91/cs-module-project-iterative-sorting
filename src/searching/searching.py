5323b1c18e79728993e922dbfc847cffafc1be77
Joshua Chinchilla <JoshuaChinchilla91@gmail.com>
2020-09-30 19:34:57 -0700

    iterative and searching tests passing

--- src/searching/searching.py
+++ src/searching/searching.py
@@ -1,5 +1,8 @@
 def linear_search(arr, target):
     # Your code here
+    for i in range(len(arr)):
+        if arr[i] == target:
+            return i
 
 
     return -1   # not found
@@ -9,6 +12,29 @@ def linear_search(arr, target):
 def binary_search(arr, target):
 
     # Your code here
+    first = 0
 
+    last = len(arr) - 1
 
-    return -1  # not found
+    completed = False
+
+    if len(arr) > 0:
+
+        while first <= last and not completed:
+            middle = (first + last) // 2
+
+            if arr[middle] == target:
+                completed = True
+                return middle
+
+            elif target < arr[middle]:
+                    last = middle - 1
+
+            elif target > arr[middle]:
+                    first = middle + 1
+
+            else:
+                completed = True
+                return  -1
+    else:
+        return -1
